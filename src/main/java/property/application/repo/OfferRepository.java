411f1b63442553d318e568357bcedc29c28a269c
Gano Koroso <gkoroso@miu.edu>
2023-06-21 21:41:54 -0500

    Offer Added

---
+++ src/main/java/property/application/repo/OfferRepository.java
@@ -0,0 +1,11 @@
+package property.application.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import property.application.model.Offer;
+
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+    List<Offer> findAllPropertyId(Long id);
+
+}
